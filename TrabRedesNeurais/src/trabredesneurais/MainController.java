76a0abb9685edb39ebb7f867581298818c690ebf
HITRON <vicgabriel17@hotmail.com>
2021-05-21 15:13:02 -0300

    - Init Commit

---
+++ TrabRedesNeurais/src/trabredesneurais/MainController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabredesneurais;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author vicga
+ */
+public class MainController implements Initializable 
+{
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        
+    }    
+}
