15c7f2b40e1b976b85032754417d3fa99357a607
HITRON <vicgabriel17@hotmail.com>
2021-05-21 16:17:51 -0300

    - Tela de treinamento montada

--- TrabRedesNeurais/src/trabredesneurais/TreinamentoController.java
+++ TrabRedesNeurais/src/trabredesneurais/TreinamentoController.java
@@ -1,27 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabredesneurais;
 
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
 
-/**
- * FXML Controller class
- *
- * @author vicga
- */
 public class TreinamentoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ToggleGroup gay;
+    @FXML
+    private JFXTextField txentrada;
+    @FXML
+    private JFXTextField txsaida;
+    @FXML
+    private JFXTextField txoculta;
+    @FXML
+    private JFXTextField txiteracoes;
+    @FXML
+    private JFXTextField txerro;
+    @FXML
+    private JFXRadioButton rblin;
+    @FXML
+    private JFXRadioButton rblog;
+    @FXML
+    private JFXTextField txaprendizagem;
+    @FXML
+    private JFXRadioButton rbhiper;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        
     }    
-    
+
+    @FXML
+    private void clkArq(ActionEvent event) 
+    {
+        
+    }
+
+    @FXML
+    private void clkTreinar(ActionEvent event) 
+    {
+        
+    }
 }
