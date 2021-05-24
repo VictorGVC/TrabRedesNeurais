52ccf57c5e705283c5d20342004251df5fb57825
MateusFS99 <mateusfsantos.pfs@gmail.com>
2021-05-24 14:33:42 -0300

    - Tela Teste

--- TrabRedesNeurais/src/trabredesneurais/TesteController.java
+++ TrabRedesNeurais/src/trabredesneurais/TesteController.java
@@ -1,27 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabredesneurais;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
-/**
- * FXML Controller class
- *
- * @author vicga
- */
 public class TesteController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TableView<?> tvdados;
+    @FXML
+    private TableView<?> tvconfusao;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
-    
+
+    @FXML
+    private void clkArq(ActionEvent event) {
+    }
+
+    @FXML
+    private void clkTreinar(ActionEvent event) {
+    }
 }
