b5d9cc435a290f77fabcd1bd1f66184e1f9daf9e
MateusFS99 <mateusfsantos.pfs@gmail.com>
2021-05-24 15:06:45 -0300

    - Atualizações Tela

--- TrabRedesNeurais/src/trabredesneurais/MainController.java
+++ TrabRedesNeurais/src/trabredesneurais/MainController.java
@@ -1,10 +1,19 @@
 package trabredesneurais;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MainController implements Initializable {
     
@@ -16,5 +25,20 @@ public class MainController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        try {
+            URL oUrl = new URL("http://www.soundjay.com/button/beep-02.wav");
+            Clip oClip = AudioSystem.getClip();
+            AudioInputStream oStream = AudioSystem.getAudioInputStream(oUrl);
+            oClip.open(oStream);
+            oClip.loop(0);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedAudioFileException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }    
 }
--- TrabRedesNeurais/src/trabredesneurais/TesteController.java
+++ TrabRedesNeurais/src/trabredesneurais/TesteController.java
@@ -1,18 +1,23 @@
 package trabredesneurais;
 
+import com.opencsv.CSVReader;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 
 public class TesteController implements Initializable {
 
+    private CSVReader csvr;
+    
     @FXML
-    private TableView<?> tvdados;
+    private TableView<List<String>> tvdados;
     @FXML
-    private TableView<?> tvconfusao;
+    private TableView<List<String>> tvconfusao;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -24,6 +29,20 @@ public class TesteController implements Initializable {
     }
 
     @FXML
-    private void clkTreinar(ActionEvent event) {
+    private void clkTestar(ActionEvent event) {
+        
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        
+        a.setTitle("Treinar");
+        a.setHeaderText("Atenção!");
+        if(csvr == null) {
+            
+            a.setContentText("Arquivo Não Aberto");
+            a.showAndWait();
+            try { clkArq(null); } catch(Exception e) {}
+        }
+        else {
+            
+        }
     }
 }
